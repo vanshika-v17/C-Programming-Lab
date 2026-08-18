2650be6410af2a6f4fe928ba1955de8b012f2626
Vanshika <vanshikav1712@gmail.com>
2026-08-18 12:56:43 +0530

    number of digits even or odd

---
+++ evendigits.c
@@ -0,0 +1,27 @@
+/*  
+Write a C code to check if a number has even digits or odd digits. Check till 9 digit no.
+Test score : INPUT 57, OUTPUT even digits , INPUT 264 , OUTPUTodd digits.
+*/
+
+#include<stdio.h>
+int main() {
+    int num;
+    printf("enter num : ");
+    scanf("%d", &num);
+    // only checking till 9 digit numbers
+
+    if( num >= 0 && num <= 999999999) {
+        if (( num >= 0 && num <= 9 ) || ( num >= 100 && num <= 999 ) || ( num >= 10000 && num <= 99999) || (  num >= 1000000 && num <= 9999999) ||( num >= 100000000 && num <= 999999999))
+        {   
+            printf("odd digits\n");
+        }
+        else {
+            printf("even digits\n"); }
+        }
+    else {
+        printf("Invalid num\n");
+    }
+
+    return 0;
+}
+
